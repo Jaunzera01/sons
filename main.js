23ee43cf35d3f7ef5ea3a904b717ff87f502f5e5
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2023-09-25 08:02:30 -0300

    ....

---
+++ main.js
@@ -0,0 +1,23 @@
+// função = tocar
+function tocar() {
+    document.querySelector ('#som_tecla_pow').play();
+ }
+ 
+ //constante = lista de botões 
+ const ListaDeTeclas = document.querySelectorALL('.,tecla');
+ 
+ //variavel =0 a infinito 
+ let cotador = 0;
+ 
+ // enquanto ou condição
+ while (contador< ListaDeTeclas.length) {
+     // acessando o elemento dentro da constante lista
+     ListaTeclas[contador].onclick = tocar;
+ 
+      //adicionado a variavel= novo valor
+      Contador = Contador +1;
+     }
+                               
+     ,
+     //console conversando comigo
+     console.log(contador);
