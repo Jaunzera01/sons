8fc84abd0f269305f6c765b685b5dc79ee75fa35
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2023-10-09 08:23:26 -0300

    chewbacca

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ function tocar(idElementoAudio) {
  }
  const ListaDeTeclas = document.querySelectorALL('.,tecla');
  
- let cotador = 0;
  
  for (contador< ListaDeTeclas.length) {
-     const tecla = ListaDeTeclas[Contador];
+     const tecla = ListaDeTeclas[Contador++];
      const classe = tecla.classList[1];
      const idAudio = `#som_${classe}`;
      tecla.onclick = function () {
@@ -17,12 +16,12 @@ function tocar(idElementoAudio) {
      }
       Contador = Contador +1;
       tecla.onkeydown = fuction () (evento) {
-        if (evento.code === 'Space'   evento.code ==='E')
+        if (evento.code === 'Space'   evento.code ==='Enter')
        tecla.classList.add('ativa');
        //console.log(evento.code==='Space' evento.code ==='Enter")
     } 
     
     tecla.onkeyup = function() {
         tecla.classList.remove('ativa');
-    }
+    } 
 }
